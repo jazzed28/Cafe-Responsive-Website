c49360c340f8cda8cb516056aee518ca60bc9f60
jazzed28 <peteson.me@gmail.com>
2018-09-11 14:25:13 +1200

    add jquery and waypoint to implement skicky header

---
+++ resources/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, { 
+        offset: '60px'
+    })
+//    var waypoint = new Waypoint({
+//      element: document.getElementById('px-offset-waypoint'),
+//      handler: function(direction) {
+//        notify('I am 25% from the top of the window')
+//      },
+//      offset: 25% 
+//    })
+});
